61f9ad000326e0c7f510a7ece641e1c17ca56b6c
rahul <shahrahul795@gmail.com>
2020-11-27 21:28:31 +0530

    before the game

--- dist/server/server.js
+++ dist/server/server.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const http_1 = __importDefault(require("http"));
 const port = 3000;
 class App {
     constructor(port) {
@@ -15,12 +15,11 @@ class App {
         app.use('/build/three.module.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/build/three.module.js')));
         app.use('/jsm/controls/OrbitControls', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')));
         app.use('/jsm/libs/stats.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/stats.module.js')));
-        app.use('/jsm/libs/dat.gui.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/dat.gui.module.js')));
         this.server = new http_1.default.Server(app);
     }
     Start() {
         this.server.listen(this.port, () => {
-            console.log(`Server listening on Port ${this.port}.`);
+            console.log(`Server listening on port ${this.port}.`);
         });
     }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,11 @@
-import http from "http"
-import path from "path"
 import express from "express"
+import path from "path"
+import http from "http"
 
 const port: number = 3000
 
 class App {
+
     private server: http.Server
     private port: number
 
@@ -15,15 +16,15 @@ class App {
         app.use('/build/three.module.js', express.static(path.join(__dirname, '../../node_modules/three/build/three.module.js')))
         app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')))
         app.use('/jsm/libs/stats.module', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/libs/stats.module.js')))
-        app.use('/jsm/libs/dat.gui.module', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/libs/dat.gui.module.js')))
         this.server = new http.Server(app);
     }
 
     public Start() {
         this.server.listen(this.port, () => {
-            console.log(`Server listening on Port ${this.port}.`)
+            console.log( `Server listening on port ${this.port}.` )
         })
     }
+
 }
 
-new App(port).Start()
+new App(port). Start()
